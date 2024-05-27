67c4f359cadc20ab0e6db51db49799d3cccc2b74
ZiadAlsaedi <ziad12399@gmail.com>
2024-05-28 00:46:01 +0300

    v1

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 let btn = document.getElementById('btn')
-
 let num = document.getElementById("num")
 let result = document.getElementById("result")
-
-
-
-
 btn.addEventListener('click',function() {
 result.innerText=(parseInt(num.value) * 0.15) + parseInt(num.value)
 })
+let btn2 = document.getElementById('btn2')
+let num2 = document.getElementById("num2")
+let result2 = document.getElementById("result2")
+btn2.addEventListener('click',function() {
+let total =(parseInt(num.value) * 0.15) + parseInt(num.value)
+let total2 =total/num2.value
+result2.innerText=total2
+})
+    
 
-  
+
 
 
